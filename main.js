931c483ed29ed3f22614eee8c1ada8363fe4f949
Le Hoai Nam <lehoainam0403@HoaiNam-Le.local>
2024-01-13 17:45:30 +0900

    Gan xong

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+console.log(navBar);
 
+// for (let i = 0; i < navBar.length; i++) {
+//   navBar[i].onclick = function () {
+//     alert(`${this.innerText}`);
+//   };
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var navBar = document.getElementsByClassName('navbar__item');
+  console.log(navBar);
+  navBar.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+      alert('Ban vua click');
+    });
+  });
+});
